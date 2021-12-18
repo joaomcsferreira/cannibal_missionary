7a13a2ddaf1da7fd16556dbf17a98cf03adf163f
Joao-MCF <marcosc024@gmail.com>
2021-12-18 14:39:05 -0300

    added cannibals, missionaries and raft builder class

---
+++ src/characters.py
@@ -0,0 +1,20 @@
+import pygame
+from os import getcwd
+
+
+class Character:
+    def __init__(self, screen, position, character) -> None:
+        self.screen = screen
+        self.position = position
+
+        self.chracter = character
+        self.character_surface = pygame.image.load(f'{getcwd()}/static/images/{character}.png')
+
+    def change_position(self, position):
+        self.position = position
+    
+    def render(self):
+        self.screen.blit(self.character_surface, self.position)
+
+    def type(self):
+        return self.chracter
